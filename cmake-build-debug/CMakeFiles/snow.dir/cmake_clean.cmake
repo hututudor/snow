e8eaa8c927e4f4867f6cab45d7bf203d2b2d4943
Tudor Hutu <hutu.tudor@yahoo.com>
2020-05-10 21:11:13 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/snow.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/snow.dir/main.cpp.obj"
+  "snow.exe"
+  "snow.exe.manifest"
+  "snow.lib"
+  "snow.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/snow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
